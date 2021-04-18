8e64e55dbf4f06331068448152785f2df01b0261
mstanto4 <mstanto4@vols.utk.edu>
2021-04-18 19:08:34 -0400

    comment faultAnalysis

--- faultImpact/faultAnalysis.py
+++ faultImpact/faultAnalysis.py
@@ -12,35 +12,43 @@ fault = open(sys.argv[1], "r")
 lines = fault.readlines()
 
 for line in lines:
+	#if line starts with t then it is the line that contains the testing information
 	if(line[0] == 't'):
 		temp = line.split(' ')
 		output = temp[len(temp)-1][0:len(temp[len(temp)-1])-1]
+	#otherwise it is a fault
 	else:
 		if('*' in line):
 			temp = line.split(' ')
 			#if node already in list
 			if(temp[2] in nodes):
 				index = nodes.index(temp[2])
+				#stuck at 0
 				if(temp[3][1] == '0'):
 					fault0[index] = fault0[index] + 1
 					diff = 0
+					#calculate difference in outputs
 					for num, x in enumerate(output):
 						if(x != temp[5][num]):
 							diff = diff + 1
 					faultOut0[index] = faultOut0[index] + diff
+				#stuck at 1
 				else:
 					fault1[index] = fault1[index] + 1
 					diff = 0
+					#calculate difference in outputs
 					for num, x in enumerate(output):
 						if(x != temp[5][num]):
 							diff = diff + 1
 					faultOut1[index] = faultOut1[index] + diff	
 			else:
 				nodes.append(temp[2])
+				#stuck at 0
 				if(temp[3][1] == '0'):
 					fault0.append(1)
 					fault1.append(0)
 					diff = 0
+					#calculate difference in outputs
 					for num, x in enumerate(output):
 						if(x != temp[5][num]):
 							diff = diff + 1
@@ -50,6 +58,7 @@ for line in lines:
 					fault1.append(1)
 					fault0.append(0)
 					diff = 0
+					#calculate difference in outputs
 					for num, x in enumerate(output):
 						if(x != temp[5][num]):
 							diff = diff + 1
@@ -61,9 +70,7 @@ for i in range(0, len(fault0)):
 	FI = (fault0[i] * faultOut0[i]) + (fault1[i] * faultOut1[i])
 	faultImpact.append(FI)
 
+#print results 
 for i in range(0, len(faultImpact)):
 	print(nodes[i] + ' ' + str(faultImpact[i]))
-
-
-
 
